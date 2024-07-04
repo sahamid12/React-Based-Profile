31673ebc9d9d315ad95a77207f79442de7f8139f
Sara Hamid <sahamid@mun.ca>
2024-07-04 14:58:23 -0300

    contacts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
 function App() {
   return (
     <>
       <Hero />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }
