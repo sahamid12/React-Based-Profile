27abf452b6938b96bbd1180d0feb21e9e56df2d8
Sara Hamid <sahamid@mun.ca>
2024-07-04 11:41:29 -0300

    Hero component made

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,69 @@
+import styles from "./HeroStyles.module.css";
+import saraImg from "../../assets/Sara.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import CV from "../../assets/Sara_Resume.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={saraImg}
+          alt="Profile picture of Sara Hamid"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          {" "}
+          Sara <br /> Hamid
+        </h1>
+        <h2>FullStack Developer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./ProjectsStyles.module.css";
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      Projects
+    </section>
+  );
+}
+
+export default Projects;
